d2fe2d5095b5dd62c800ccefede722f3b1f9f59a
Patrick Carey <paddy@shopkeep.com>
2015-11-26 23:55:27 +0000

    added basic noarg and --help tests for all available commands

---
+++ tests/commands/test_cmd_logs.py
@@ -0,0 +1,22 @@
+# third-party imports
+from click.testing import CliRunner
+
+# local imports
+from shpkpr.cli import cli
+
+
+def test_no_args():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['logs'])
+
+    assert result.exit_code == 2
+    assert 'Usage:' in result.output
+
+
+def test_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['logs', '--help'])
+
+    assert result.exit_code == 0
+    assert 'Usage:' in result.output
+    assert 'Tail a file in a mesos task\'s sandbox.' in result.output
---
+++ tests/commands/test_cmd_scale.py
@@ -0,0 +1,22 @@
+# third-party imports
+from click.testing import CliRunner
+
+# local imports
+from shpkpr.cli import cli
+
+
+def test_no_args():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['scale'])
+
+    assert result.exit_code == 2
+    assert 'Usage:' in result.output
+
+
+def test_help():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['scale', '--help'])
+
+    assert result.exit_code == 0
+    assert 'Usage:' in result.output
+    assert 'Scale application resources to specified levels.' in result.output
