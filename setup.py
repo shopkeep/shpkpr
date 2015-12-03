48a37b588f0e385d6640938e7afc95874815b92c
Patrick Carey <paddy@shopkeep.com>
2015-12-03 13:49:41 +0000

    ensure JSON files are included in package

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
     author_email='developers@shopkeep.com',
     url='https://github.com/shopkeep/shpkpr',
     packages=['shpkpr', 'shpkpr.commands', 'shpkpr.marathon'],
+    package_data={'shpkpr': ['marathon/schema/*.json']},
     include_package_data=True,
     install_requires=[
         'cached-property==1.3.0',
