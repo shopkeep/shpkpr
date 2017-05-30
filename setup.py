ae3997b1980dcc58c0d067021d3469a34521e4d7
Patrick Carey <paddy@shopkeep.com>
2017-05-30 12:47:36 +0100

    Upgrade chronos-python to 1.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'PyYAML>=3.10.0',
         'cached-property>=1.3.0, <2',
-        'chronos-python>=1.0.0',
+        'chronos-python>=1.1.0',
         'click>=6.0, <7',
         'jinja2>=2.6, <3',
         'jsonschema>=2.6, <3',
