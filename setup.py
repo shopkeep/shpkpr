8a2274d25eff295596a8b691989f09dfd4e9795e
Patrick Carey <paddy@shopkeep.com>
2017-10-31 17:06:22 +0000

    Bump to version 3.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except IOError:
 
 setup(
     name='shpkpr',
-    version='3.0.0',
+    version='3.1.0',
     description='shpkpr is a command-line tool designed to manage applications running on Marathon',
     long_description=readme,
     author='ShopKeep.com Inc.',
